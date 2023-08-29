f84f648fc38dd7503482673a09722b8ad7bf0202
Robert Leung <rleung84@uwo.ca>
2023-08-28 22:25:38 -0400

    updates to navbar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
+import "./style.css";
+
 let content = document.querySelector(".content");
 let nav = document.createElement("div");
+nav.classList.add("nav");
 let home = document.createElement("div");
 home.innerText = "Home";
+home.classList.add("nav-selected");
 let menu = document.createElement("div");
 menu.innerText = "Menu";
-let contact = document.createElement("contact");
+let contact = document.createElement("div");
 contact.innerText = "Contact";
 nav.append(home, menu, contact);
 content.append(nav);
